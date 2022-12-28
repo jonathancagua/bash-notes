0d08f2180fd6bec46813e96c0eedc72073fd20ba
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-12-28 13:50:43 -0500

    ADD: operadores

---
+++ 3_operador.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#operators
+A=10
+B=4
+echo "BASIC ARITHMETIC OPERATORS"
+echo -e "inputs: A=$A , B=$B\n"
+echo "Addition operator: A+B=" $((A+B))
+echo "Subtraction operator: A-B=" $((A-B))
+echo "Multiplication operator: A*B=" $((A*B))
+echo "Division operator: A/B=" $((A/B))
+echo "Module operator: A%B=" $((A%B))
+echo  -e "\n------------------------------------------"
