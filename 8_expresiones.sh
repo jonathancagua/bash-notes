113ba1c6e0e7992f127c41239cf18a55092f1ed9
Jonathan Cagua <jonathan.cagua@gmail.com>
2023-01-02 19:17:16 -0500

    ADD: expresiones

---
+++ 8_expresiones.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+option=0
+backupName=""
+clave=""
+
+echo "Programa Utilidades Postgres"
+# Acepta el ingreso de información de solo un caracter
+read -n1 -p "Ingresar una opción:" option
+echo -e "\n"
+read -n10 -p "Ingresar el nombre del archivo del backup:" backupName
+echo -e "\n"
+echo "Opción:$option , backupName:$backupName"
+read -s -p "Clave:" clave
+echo "Clave: $clave"
+
+#Validación Fecha Nacimiento
+if [[ $fechaNacimiento =~ $fechaNacimientoRegex ]]; then
+    echo "Fecha Nacimiento $fechaNacimiento válida"
+else
+    echo "Fecha Nacimiento $fechaNacimiento inválida"
+fi
